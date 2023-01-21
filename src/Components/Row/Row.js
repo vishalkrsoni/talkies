96e89e1ebe37ad17c4a8e667930c6aeec518d982
vishalkrsoni <vishal.sony1@gmail.com>
2023-01-21 09:26:18 +0530

    row fixed

--- src/Components/Row/Row.js
+++ src/Components/Row/Row.js
@@ -5,7 +5,7 @@ import "./Row.css";
 const Row = ({ title, fetchUrl, isLargeRow = false }) => {
   const [movies, setMovies] = useState([]);
 
-  //   const BASE_URL = "https://image.tmdb.org/t/p/original/";
+    const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   useEffect(() => {
     async function fetchData() {
@@ -17,10 +17,25 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
   }, [fetchUrl]);
   console.log(movies);
   return (
-    <div className="row">
-      <h2>{title}</h2>
+    <div className='row'>
+        <h2>{ title }</h2>
+
+        <div className="row__posters">
+            {movies.map((movie) => (
+                (isLargeRow && movie.poster_path) ||
+                (!isLargeRow && movie.backdrop_path)) && (
+                    <img key={movie.id}
+                        className = {`row__poster ${isLargeRow && 'row__posterLarge'}`}
+                        src={`${BASE_URL}${
+                            isLargeRow ? movie.poster_path : movie.backdrop_path
+                        }`}
+                        alt = {movie.name} 
+                    />
+                )
+            )}
+        </div>
     </div>
-  );
-};
+  )
+}
 
 export default Row;
